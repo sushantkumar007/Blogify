06fdb6c653dc260a92a0ae7f1ec6fbe02fecb7ee
sushantkumar51 <131727757+sushantkumar51@users.noreply.github.com>
2024-01-21 05:37:31 +0000

    changes are done

--- MegaProject/src/App.jsx
+++ MegaProject/src/App.jsx
@@ -1,12 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux'
+import { login, logout } from './store/authSlice'
+import authService from './appwrite/auth'
+import { Header, Footer } from './components/index'
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
 
-  return (
-    <>
-     <h1>React Mega Project App</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+      
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          Todo: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (<h1>Loading...</h1>)
+
 }
 
 export default App
