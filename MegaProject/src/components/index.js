fd19ae197463e9996b2446b8198576a0a1e252b6
sushantkumar51 <131727757+sushantkumar51@users.noreply.github.com>
2024-01-21 14:47:10 +0000

    changes are done

--- MegaProject/src/components/index.js
+++ MegaProject/src/components/index.js
@@ -1,7 +1,12 @@
+import Container from './container/Container'
 import Header from './Header/Header'
+import LogoutBtn from './Header/LogoutBtn'
 import Footer from './Footer/Footer'
+import Logo from './Logo'
 
 export { 
     Header, 
-    Footer 
+    Footer,
+    Container,
+    Logo,
 }
