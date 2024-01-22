472dd1d99fa12ec0110dbd37fd0539e76a0f10d5
sushantkumar51 <131727757+sushantkumar51@users.noreply.github.com>
2024-01-22 09:16:47 +0000

    changes are done

--- MegaProject/src/components/index.js
+++ MegaProject/src/components/index.js
@@ -3,10 +3,19 @@ import Header from './Header/Header'
 import LogoutBtn from './Header/LogoutBtn'
 import Footer from './Footer/Footer'
 import Logo from './Logo'
+import Button from './Button'
+import Input from './Input'
+import PostCard from './PostCard'
+import Select from './Select'
 
 export { 
     Header, 
     Footer,
     Container,
     Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    PostCard,
+    Select
 }
